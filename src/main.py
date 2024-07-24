25d7e06a429e65c85cb4f5d5af3179c491421ee4
Cristina Melnic <103945852+cristina-v-melnic@users.noreply.github.com>
2024-07-24 14:23:53 +0200

    theme added

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from data import get_data
 
 def main():
     st.set_page_config(layout="wide")
-    st.set_page_config(theme="light")
+    st.set_page_config(base="light")
 
     st.markdown("# 🌍 Global Life Quality and Economic Analysis 📊")
 
